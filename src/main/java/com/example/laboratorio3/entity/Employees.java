d5afcf3c9f97d4b163b79132dedf918a094080a4
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-04-26 20:02:14 -0500

    Atributos ManyToOne a la entidad Employees

--- src/main/java/com/example/laboratorio3/entity/Employees.java
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -1,13 +1,11 @@
 package com.example.laboratorio3.entity;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
+@Table(name="employees")
 public class Employees {
     @Id
     @Column(nullable = false)
@@ -23,114 +21,19 @@ public class Employees {
     @Column(nullable = false)
     private LocalDateTime hire_date;
     @Column(nullable = false)
-    private String job_id;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
+
     private BigDecimal salary;
     private  BigDecimal commission_pct;
     private Integer manager_id;
-    private  Integer department_id;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
     private Integer enabled;
 
-    public Integer getEmployee_id() {
-        return employee_id;
-    }
-
-    public void setEmployee_id(Integer employee_id) {
-        this.employee_id = employee_id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public LocalDateTime getHire_date() {
-        return hire_date;
-    }
-
-    public void setHire_date(LocalDateTime hire_date) {
-        this.hire_date = hire_date;
-    }
-
-    public String getJob_id() {
-        return job_id;
-    }
-
-    public void setJob_id(String job_id) {
-        this.job_id = job_id;
-    }
-
-    public BigDecimal getSalary() {
-        return salary;
-    }
-
-    public void setSalary(BigDecimal salary) {
-        this.salary = salary;
-    }
-
-    public BigDecimal getCommission_pct() {
-        return commission_pct;
-    }
-
-    public void setCommission_pct(BigDecimal commission_pct) {
-        this.commission_pct = commission_pct;
-    }
-
-    public Integer getManager_id() {
-        return manager_id;
-    }
-
-    public void setManager_id(Integer manager_id) {
-        this.manager_id = manager_id;
-    }
-
-    public Integer getDepartment_id() {
-        return department_id;
-    }
-
-    public void setDepartment_id(Integer department_id) {
-        this.department_id = department_id;
-    }
-
-    public Integer getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Integer enabled) {
-        this.enabled = enabled;
-    }
 }
