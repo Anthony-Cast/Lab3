aa82eae3c15df5c01bc9e3edb8b1908f54465b9c
LiziAC05 <lizbeth_aqcast@hotmail.com>
2021-04-26 20:15:37 -0500

    Primeras modificaciones

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -1,35 +1,67 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
-    //COMPLETAR
+    @Autowired
+    EmployeesRepository employeesRepository;
 
-    public String listaEmployee(   ){
-        //COMPLETAR
+    @GetMapping
+    public String listaEmployee(Model model){
+        model.addAttribute("listaEmpleados", employeesRepository.findAll());
+        return "employee/lista";
     }
 
+    @GetMapping
     public String nuevoEmployeeForm( ) {
-        //COMPLETAR
+       return "employee/newFrm";
     }
 
-
-    public String guardarEmployee() {
-        //COMPLETAR
+    @PostMapping
+    public String guardarEmployee(Employees employees, RedirectAttributes redirectAttributes) {
+        if (employees.getEmployee_id() == 0) {
+            redirectAttributes.addFlashAttribute("msg", "Empleado creado exitosamente");
+        } else {
+            redirectAttributes.addFlashAttribute("msg", "Empleado actualizado exitosamente");
+        }
+        employeesRepository.save(employees);
+        return "redirect:/employees";
     }
 
-
-    public String editarEmployee() {
-        //COMPLETAR
+    @GetMapping
+    public String editarEmployee(Model model, @RequestParam("id") int id) {
+        Optional<Employees> employeesOptional = employeesRepository.findById(id);
+        if (employeesOptional.isPresent()) {
+            Employees employees = employeesOptional.get();
+            model.addAttribute("employee", employees);
+            return "employee/editFrm";
+        } else {
+            return "redirect:/employees";
+        }
     }
 
-
-    public String borrarEmpleado() {
-
-       //COMPLETAR
-
+    @GetMapping
+    public String borrarEmpleado(Model model, @RequestParam("id") int id, RedirectAttributes redirectAttributes) {
+        Optional<Employees> employeesOptional = employeesRepository.findById(id);
+        if (employeesOptional.isPresent()) {
+            employeesRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("msg", "Empleado borrado exitosamente");
+        }
+            return "redirect:/employees";
     }
 
     //COMPLETAR
