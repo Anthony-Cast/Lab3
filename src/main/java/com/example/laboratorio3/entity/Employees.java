39d5cdf04c7f501f80eb92d6f3d966d880bdb123
AnthonyCastañeda <acastanedap@pucp.edu.pe>
2021-04-26 19:13:01 -0500

    Lab3

---
+++ src/main/java/com/example/laboratorio3/entity/Employees.java
@@ -0,0 +1,12 @@
+package com.example.laboratorio3.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Employees {
+
+
+}
